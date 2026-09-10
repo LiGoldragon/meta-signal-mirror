1dedbb34acdf51ce14b11e129e7b93671acfa6ce
li <li@goldragon.criome.net>
2026-09-10 08:57:47 +0200

    feat: migrate owner mirror contract to current Signal

---
+++ src/generated/signal.rs
@@ -0,0 +1,184 @@
+#![allow(dead_code, non_camel_case_types, non_snake_case)]
+#[rustfmt::skip]
+pub type ConfigureRequest = DaemonConfiguration;
+#[rustfmt::skip]
+pub type StoreRetirement = signal_mirror::StoreName;
+#[rustfmt::skip]
+#[derive(rkyv::Archive, rkyv::Serialize, rkyv::Deserialize, Clone, Debug, PartialEq)]
+#[cfg_attr(
+    feature = "datom",
+    derive(datom_codec::Datomizable, datom_codec::Compositional)
+)]
+pub struct LocalSocketBinding {
+    pub socket_path: signal_standard::SocketPath,
+    pub socket_mode: SocketMode,
+}
+#[rustfmt::skip]
+#[derive(rkyv::Archive, rkyv::Serialize, rkyv::Deserialize, Clone, Debug, PartialEq)]
+#[cfg_attr(
+    feature = "datom",
+    derive(datom_codec::Datomizable, datom_codec::Compositional)
+)]
+pub enum RetentionScope {
+    AllStores,
+    Store(signal_mirror::StoreName),
+}
+#[rustfmt::skip]
+#[derive(rkyv::Archive, rkyv::Serialize, rkyv::Deserialize, Clone, Debug, PartialEq)]
+#[cfg_attr(
+    feature = "datom",
+    derive(datom_codec::Datomizable, datom_codec::Compositional)
+)]
+pub struct RegistryQuery {}
+#[rustfmt::skip]
+pub type StoragePath = String;
+#[rustfmt::skip]
+pub type RegistrationReceipt = signal_mirror::StoreName;
+#[rustfmt::skip]
+pub type RetirementReceipt = signal_mirror::StoreName;
+#[rustfmt::skip]
+pub type RegisteredStore = signal_mirror::StoreName;
+#[rustfmt::skip]
+pub type SocketMode = i64;
+#[rustfmt::skip]
+pub type MetaSocketBinding = LocalSocketBinding;
+#[rustfmt::skip]
+#[derive(rkyv::Archive, rkyv::Serialize, rkyv::Deserialize, Clone, Debug, PartialEq)]
+#[cfg_attr(
+    feature = "datom",
+    derive(datom_codec::Datomizable, datom_codec::Compositional)
+)]
+pub struct RegistryListing {
+    pub registered_store_vector: std::vec::Vec<RegisteredStore>,
+}
+#[rustfmt::skip]
+pub type WorkingSocketBinding = LocalSocketBinding;
+#[rustfmt::skip]
+#[derive(rkyv::Archive, rkyv::Serialize, rkyv::Deserialize, Clone, Debug, PartialEq)]
+#[cfg_attr(
+    feature = "datom",
+    derive(datom_codec::Datomizable, datom_codec::Compositional)
+)]
+pub enum OrderRejectionReason {
+    StoreUnknown,
+    LedgerFault,
+    StoreAlreadyRegistered,
+    StoreNameInvalid,
+}
+#[rustfmt::skip]
+#[derive(rkyv::Archive, rkyv::Serialize, rkyv::Deserialize, Clone, Debug, PartialEq)]
+#[cfg_attr(
+    feature = "datom",
+    derive(datom_codec::Datomizable, datom_codec::Compositional)
+)]
+pub enum RetentionRule {
+    KeepLatestCheckpoints(CheckpointKeepCount),
+    KeepEverything,
+}
+#[rustfmt::skip]
+#[derive(rkyv::Archive, rkyv::Serialize, rkyv::Deserialize, Clone, Debug, PartialEq)]
+#[cfg_attr(
+    feature = "datom",
+    derive(datom_codec::Datomizable, datom_codec::Compositional)
+)]
+pub struct RetentionOrder {
+    pub retention_scope: RetentionScope,
+    pub retention_rule: RetentionRule,
+}
+#[rustfmt::skip]
+pub type ConfigurationReceipt = DaemonConfiguration;
+#[rustfmt::skip]
+#[derive(rkyv::Archive, rkyv::Serialize, rkyv::Deserialize, Clone, Debug, PartialEq)]
+#[cfg_attr(
+    feature = "datom",
+    derive(datom_codec::Datomizable, datom_codec::Compositional)
+)]
+pub struct DaemonConfiguration {
+    pub storage_path: StoragePath,
+    pub working_socket_binding: WorkingSocketBinding,
+    pub meta_socket_binding: MetaSocketBinding,
+    pub network_endpoint: signal_standard::NetworkEndpoint,
+}
+#[rustfmt::skip]
+pub type CheckpointKeepCount = i64;
+#[rustfmt::skip]
+#[derive(rkyv::Archive, rkyv::Serialize, rkyv::Deserialize, Clone, Debug, PartialEq)]
+#[cfg_attr(
+    feature = "datom",
+    derive(datom_codec::Datomizable, datom_codec::Compositional)
+)]
+pub struct OrderRejection {
+    pub order_rejection_reason: OrderRejectionReason,
+    pub rejection_detail: RejectionDetail,
+}
+#[rustfmt::skip]
+#[derive(rkyv::Archive, rkyv::Serialize, rkyv::Deserialize, Clone, Debug, PartialEq)]
+#[cfg_attr(
+    feature = "datom",
+    derive(datom_codec::Datomizable, datom_codec::Compositional)
+)]
+pub struct ConfigurationWrite {
+    pub configuration_archive_path: ConfigurationArchivePath,
+    pub daemon_configuration: DaemonConfiguration,
+}
+#[rustfmt::skip]
+pub type RejectionDetail = String;
+#[rustfmt::skip]
+#[derive(rkyv::Archive, rkyv::Serialize, rkyv::Deserialize, Clone, Debug, PartialEq)]
+#[cfg_attr(
+    feature = "datom",
+    derive(datom_codec::Datomizable, datom_codec::Compositional)
+)]
+pub struct StoreRegistration {
+    pub store_name: signal_mirror::StoreName,
+    pub content_addressing: ContentAddressing,
+}
+#[rustfmt::skip]
+#[derive(rkyv::Archive, rkyv::Serialize, rkyv::Deserialize, Clone, Debug, PartialEq)]
+#[cfg_attr(
+    feature = "datom",
+    derive(datom_codec::Datomizable, datom_codec::Compositional)
+)]
+pub enum ContentAddressing {
+    Opaque,
+    SemaVersionedLog,
+}
+#[rustfmt::skip]
+#[derive(rkyv::Archive, rkyv::Serialize, rkyv::Deserialize, Clone, Debug, PartialEq)]
+#[cfg_attr(
+    feature = "datom",
+    derive(datom_codec::Datomizable, datom_codec::Compositional)
+)]
+pub struct RetentionReceipt {
+    pub retention_scope: RetentionScope,
+    pub retention_rule: RetentionRule,
+}
+#[rustfmt::skip]
+pub type ConfigurationArchivePath = String;
+#[rustfmt::skip]
+#[derive(rkyv::Archive, rkyv::Serialize, rkyv::Deserialize, Clone, Debug, PartialEq)]
+#[cfg_attr(
+    feature = "datom",
+    derive(datom_codec::Datomizable, datom_codec::Compositional)
+)]
+pub enum Query {
+    RetireStore(StoreRetirement),
+    RegisterStore(StoreRegistration),
+    Configure(ConfigureRequest),
+    ObserveRegistry(RegistryQuery),
+    SetRetention(RetentionOrder),
+}
+#[rustfmt::skip]
+#[derive(rkyv::Archive, rkyv::Serialize, rkyv::Deserialize, Clone, Debug, PartialEq)]
+#[cfg_attr(
+    feature = "datom",
+    derive(datom_codec::Datomizable, datom_codec::Compositional)
+)]
+pub enum Response {
+    StoreRegistered(RegistrationReceipt),
+    RegistryObserved(RegistryListing),
+    RetentionSet(RetentionReceipt),
+    OrderRejected(OrderRejection),
+    StoreRetired(RetirementReceipt),
+    Configured(ConfigurationReceipt),
+}
--- tests/round_trip.rs
+++ tests/round_trip.rs
@@ -1,196 +1,42 @@
-use meta_signal_mirror::*;
-
-fn requests() -> Vec<(z2VWt5, &'static str)> {
-    vec![
-        (
-            z2VWt5::z2VXXR(z2VWZw::new(signal_mirror::schema::lib::z2Ve8p::new(
-                "mirror-aab".to_owned(),
-            ))),
-            "RetireStore",
-        ),
-        (
-            z2VWt5::z2VWC2(z2VWBn {
-                field_0: signal_mirror::schema::lib::z2Ve8p::new("mirror-aab".to_owned()),
-                field_1: z2VMYP::z2Vf8Y,
-            }),
-            "RegisterStore",
-        ),
-        (
-            z2VWt5::z2VNSh(z2VR1a::new(z2VXab {
-                field_0: z2VPES::new("fixture".to_owned()),
-                field_1: z2VM9q::new(z2VRXG {
-                    field_0: signal_standard::schema::lib::z2VXNY::new("socket-aab".to_owned()),
-                    field_1: z2VQot::new(7),
-                }),
-                field_2: z2VUYq::new(z2VRXG {
-                    field_0: signal_standard::schema::lib::z2VXNY::new("socket-aab".to_owned()),
-                    field_1: z2VQot::new(7),
-                }),
-                field_3: signal_standard::schema::lib::z2VaVE {
-                    field_0: signal_standard::schema::lib::z2VLyh::new("host-aab".to_owned()),
-                    field_1: signal_standard::schema::lib::z2VQaE::new(7474),
-                },
-            })),
-            "Configure",
-        ),
-        (z2VWt5::z2VMxr(z2VXxR {}), "ObserveRegistry"),
-        (
-            z2VWt5::z2Vctc(z2VXLU {
-                field_0: z2VTJB::z2VcDy,
-                field_1: z2VWXC::z2VXbW(z2VQrL::new(7)),
-            }),
-            "SetRetention",
-        ),
-    ]
-}
-
-fn replies() -> Vec<(z2VUH6, &'static str)> {
-    vec![
-        (
-            z2VUH6::z2VSig(z2VbpU::new(signal_mirror::schema::lib::z2Ve8p::new(
-                "mirror-aab".to_owned(),
-            ))),
-            "StoreRegistered",
-        ),
-        (
-            z2VUH6::z2VN4H(z2VQWA::new(vec![z2VT8Z::new(
-                signal_mirror::schema::lib::z2Ve8p::new("mirror-aab".to_owned()),
-            )])),
-            "RegistryObserved",
-        ),
-        (
-            z2VUH6::z2Vedh(z2VP3W {
-                field_0: z2VTJB::z2VcDy,
-                field_1: z2VWXC::z2VXbW(z2VQrL::new(7)),
-            }),
-            "RetentionSet",
-        ),
-        (
-            z2VUH6::z2VXCa(z2Vbcq {
-                field_0: z2VYSE::z2VLwk,
-                field_1: z2VbDA::new("fixture".to_owned()),
-            }),
-            "OrderRejected",
-        ),
-        (
-            z2VUH6::z2VdCD(z2Vcin::new(signal_mirror::schema::lib::z2Ve8p::new(
-                "mirror-aab".to_owned(),
-            ))),
-            "StoreRetired",
-        ),
-        (
-            z2VUH6::z2VQdi(z2VNz1::new(z2VXab {
-                field_0: z2VPES::new("fixture".to_owned()),
-                field_1: z2VM9q::new(z2VRXG {
-                    field_0: signal_standard::schema::lib::z2VXNY::new("socket-aab".to_owned()),
-                    field_1: z2VQot::new(7),
-                }),
-                field_2: z2VUYq::new(z2VRXG {
-                    field_0: signal_standard::schema::lib::z2VXNY::new("socket-aab".to_owned()),
-                    field_1: z2VQot::new(7),
-                }),
-                field_3: signal_standard::schema::lib::z2VaVE {
-                    field_0: signal_standard::schema::lib::z2VLyh::new("host-aab".to_owned()),
-                    field_1: signal_standard::schema::lib::z2VQaE::new(7474),
-                },
-            })),
-            "Configured",
-        ),
-    ]
-}
-
-fn exchange(epoch: u64) -> signal_frame::ExchangeIdentifier {
-    signal_frame::ExchangeIdentifier::new(
-        signal_frame::SessionEpoch::new(epoch),
-        signal_frame::ExchangeLane::Connector,
-        signal_frame::LaneSequence::first(),
-    )
-}
+use meta_signal_mirror::{Query, RegistryListing, RegistryQuery, Response};
 
 #[test]
-fn every_request_round_trips_through_the_bound_frame() {
-    for (request, _head) in requests() {
-        let expected = request.clone();
-        let encoded = request
-            .encode_request_frame(exchange(61))
-            .expect("request frame encodes");
-        let (decoded_exchange, decoded) =
-            ContractMarker::decode_single_request(&encoded).expect("request frame decodes");
-        assert_eq!(decoded_exchange, exchange(61));
-        assert_eq!(decoded, expected);
-    }
-}
-
-#[test]
-fn every_reply_has_bound_frame_and_rkyv_behavior() {
-    for (reply, _head) in replies() {
-        let expected = reply.clone();
-        let encoded = reply
-            .clone()
-            .encode_reply_frame(exchange(63))
-            .expect("reply frame encodes");
-        ContractMarker::decode_frame(&encoded).expect("reply frame decodes");
-        let archive = rkyv::to_bytes::<rkyv::rancor::Error>(&reply).expect("reply archives");
-        let recovered =
-            rkyv::from_bytes::<z2VUH6, rkyv::rancor::Error>(&archive).expect("reply recovers");
-        assert_eq!(recovered, expected);
-    }
-}
-
-#[cfg(feature = "dotos-text")]
-#[test]
-fn every_root_round_trips_through_dotos_with_visible_heads() {
-    use dotos::{DotosEncode, DotosSource};
-    for (request, head) in requests() {
-        let text = request.to_dotos();
-        assert!(text.starts_with(&format!("{head}.")), "{text}");
-        assert_eq!(
-            DotosSource::new(&text)
-                .parse::<z2VWt5>()
-                .expect("request Dotos decodes"),
-            request
-        );
-    }
-    for (reply, head) in replies() {
-        let text = reply.to_dotos();
-        assert!(text.starts_with(&format!("{head}.")), "{text}");
-        assert_eq!(
-            DotosSource::new(&text)
-                .parse::<z2VUH6>()
-                .expect("reply Dotos decodes"),
-            reply
-        );
-    }
-}
-
-#[test]
-fn daemon_configuration_survives_the_binary_startup_archive() {
-    let configuration = match requests()
-        .into_iter()
-        .find(|(_, head)| *head == "Configure")
-        .expect("configure fixture")
-        .0
-    {
-        z2VWt5::z2VNSh(request) => request.into_payload(),
-        _ => unreachable!("configure fixture has configure variant"),
-    };
-    let directory = tempfile::tempdir().expect("temp dir");
-    let path = directory.path().join("mirror-configuration.rkyv");
-    configuration
-        .write_binary_file(&path)
-        .expect("write binary configuration");
-    let decoded = z2VXab::from_binary_path(&path).expect("decode binary configuration");
-    assert_eq!(decoded, configuration);
-}
-
-#[test]
-fn socket_mode_conversion_is_checked() {
+fn current_meta_query_and_response_round_trip_as_fresh_bytes() {
+    let query = Query::ObserveRegistry(RegistryQuery {});
+    let bytes = rkyv::to_bytes::<rkyv::rancor::Error>(&query).expect("archive");
     assert_eq!(
-        z2VQot::new(u64::from(u32::MAX)).into_u32().expect("fits"),
-        u32::MAX
+        rkyv::from_bytes::<Query, rkyv::rancor::Error>(&bytes).expect("restore"),
+        query
     );
+    let response = Response::RegistryObserved(RegistryListing {
+        registered_store_vector: vec![],
+    });
+    let bytes = rkyv::to_bytes::<rkyv::rancor::Error>(&response).expect("archive");
     assert_eq!(
-        z2VQot::new(u64::from(u32::MAX) + 1).into_u32(),
-        Err(SocketModeRangeError)
+        rkyv::from_bytes::<Response, rkyv::rancor::Error>(&bytes).expect("restore"),
+        response
     );
+}
+
+#[test]
+fn malformed_archive_is_rejected() {
+    assert!(rkyv::from_bytes::<Query, rkyv::rancor::Error>(&[0, 1, 2]).is_err());
+}
+
+#[cfg(feature = "datom")]
+#[test]
+fn current_meta_query_round_trips_as_datom() {
+    use datom_codec::{Actualizing, Budget, Datomizable, Potential};
+    use protos::{Protosizable, ReaderBudget, Textualizable};
+    let value = Query::ObserveRegistry(RegistryQuery {});
+    let text = value.datomize(vec![]).protosize().textualize();
+    let restored = Potential::<Query>::from(text)
+        .actualize(&mut Budget {
+            remaining: 1024,
+            reader: ReaderBudget { remaining: 1024 },
+            depth: 0,
+            maximum_depth: 1024,
+        })
+        .expect("actualize");
+    assert_eq!(restored, value);
 }
